76bfb097956ee72105d4986e3948f6bb410388b6
itsfunny <18757883747@163.com>
2023-12-27 17:51:33 +0800

    rc

--- src/storage/db/memory.rs
+++ src/storage/db/memory.rs
@@ -1,30 +1,37 @@
 use crate::error::IndexerResult;
 use crate::storage::db::DB;
 use rusty_leveldb::WriteBatch;
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 #[derive(Default, Clone)]
 pub struct MemoryDB {
-    datas: HashMap<Vec<u8>, Vec<u8>>,
+    datas: Rc<RefCell<HashMap<Vec<u8>, Vec<u8>>>>,
 }
 
+unsafe impl Send for MemoryDB {}
+unsafe impl Sync for MemoryDB {}
 impl DB for MemoryDB {
     fn set(&mut self, key: &[u8], value: &[u8]) -> IndexerResult<()> {
-        self.datas.insert(key.to_vec(), value.to_vec());
+        let mut data = self.datas.borrow_mut();
+        data.insert(key.to_vec(), value.to_vec());
         Ok(())
     }
 
     fn get(&mut self, key: &[u8]) -> IndexerResult<Option<Vec<u8>>> {
-        Ok(self.datas.get(key).cloned())
+        let mut data = self.datas.borrow_mut();
+        Ok(data.get(key).cloned())
     }
 
     fn write_batch(&mut self, batch: WriteBatch, sync: bool) -> IndexerResult<()> {
+        let mut data = self.datas.borrow_mut();
         batch.iter().for_each(|(k, v)| {
             if v.is_none() {
-                self.datas.remove(k);
+                data.remove(k);
             } else {
                 let v = v.unwrap().to_vec();
-                self.datas.insert(k.to_vec(), v);
+                data.insert(k.to_vec(), v);
             }
         });
         Ok(())
@@ -41,7 +48,8 @@ impl DB for MemoryDB {
         VF: Fn(Vec<u8>) -> Option<V>,
     {
         let mut ret = vec![];
-        for (k, v) in self.datas.iter() {
+        let mut data = self.datas.borrow_mut();
+        for (k, v) in data.iter() {
             if k.starts_with(prefix) {
                 let v = vf(v.clone());
                 if v.is_some() {
