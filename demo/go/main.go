6a094e4df3f2bf620be90ba22c7c721c84a96d5c
itsfunny <18757883747@163.com>
2023-12-29 12:57:39 +0800

    rm

--- demo/go/main.go
+++ demo/go/main.go
@@ -30,17 +30,13 @@ func main() {
 	println(2)
 	time.Sleep(time.Second * 10)
 
-	// 调用 Rust 函数
 	rustByteArray := C.get_data()
-	// 将 Rust 结构体映射到 Go 结构体
 	goByteArray := ByteArray{
 		Data:   rustByteArray.data,
 		Length: rustByteArray.length,
 	}
-	// 将 C 字节数组转换为 Go 字节数组
 	goData := C.GoBytes(unsafe.Pointer(goByteArray.Data), C.int(goByteArray.Length))
 
-	// 输出结果
 	fmt.Printf("Data: %s\n", goData)
 	fmt.Printf("Length: %d\n", goByteArray.Length)
 	println(3)
